94646f302f3f010c32d410e978aa19dce9df8197
lukehebe <53823266+lukehebe@users.noreply.github.com>
2023-11-05 21:20:07 -0500

    Update Lab9.py

--- Lab9.py
+++ Lab9.py
@@ -1,17 +1,17 @@
 import requests
 
-# Define the URL of your Node.js API
-api_url = "http://your-node-api-url-here"
+
+api_url = "http://localhost:3000/blue"
 
 try:
-    # Send a GET request to the Node.js API
+ 
     response = requests.get(api_url)
 
-    # Check if the request was successful (status code 200)
+    
     if response.status_code == 200:
         data = response.json()
         
-        # Iterate through the list of widgets and print their names and colors
+       
         for widget in data:
             widget_name = widget["name"]
             widget_color = widget["color"]
